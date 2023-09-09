bac2f41ce1f51d93ba5e327f310b49befb4b5f2f
yunniw2001 <2025151726@qq.com>
2023-09-09 16:58:42 +0800

    Initial commit

---
+++ lab1/lab1.py
@@ -0,0 +1,288 @@
+""" CS5340 Lab 1: Belief Propagation and Maximal Probability
+See accompanying PDF for instructions.
+
+Name: WU YIHANG
+Email: e1216288@u.nus.edu
+Student ID: A0285643W
+"""
+
+import copy
+from typing import List
+
+import numpy as np
+
+from factor import Factor, index_to_assignment, assignment_to_index, generate_graph_from_factors, \
+    visualize_graph
+
+
+"""For sum product message passing"""
+def factor_product(A, B):
+    """Compute product of two factors.
+
+    Suppose A = phi(X_1, X_2), B = phi(X_2, X_3), the function should return
+    phi(X_1, X_2, X_3)
+    """
+    if A.is_empty():
+        return B
+    if B.is_empty():
+        return A
+
+    # Create output factor. Variables should be the union between of the
+    # variables contained in the two input factors
+    out = Factor()
+    out.var = np.union1d(A.var, B.var) # find the union, find all x_i
+
+    # Compute mapping between the variable ordering between the two factors
+    # and the output to set the cardinality
+    out.card = np.zeros(len(out.var), np.int64)
+    mapA = np.argmax(out.var[None, :] == A.var[:, None], axis=-1)
+    mapB = np.argmax(out.var[None, :] == B.var[:, None], axis=-1)
+    out.card[mapA] = A.card
+    out.card[mapB] = B.card
+
+    # For each assignment in the output, compute which row of the input factors
+    # it comes from
+    out.val = np.zeros(np.prod(out.card))
+    assignments = out.get_all_assignments()
+    idxA = assignment_to_index(assignments[:, mapA], A.card)
+    idxB = assignment_to_index(assignments[:, mapB], B.card)
+
+    """ YOUR CODE HERE
+    You should populate the .val field with the factor product
+    Hint: The code for this function should be very short (~1 line). Try to
+      understand what the above lines are doing, in order to implement
+      subsequent parts.
+    """
+    return out
+
+
+def factor_marginalize(factor, var):
+    """Sums over a list of variables.
+
+    Args:
+        factor (Factor): Input factor
+        var (List): Variables to marginalize out
+
+    Returns:
+        out: Factor with variables in 'var' marginalized out.
+    """
+    out = Factor()
+
+    """ YOUR CODE HERE
+    Marginalize out the variables given in var
+    """
+
+    return out
+
+
+def observe_evidence(factors, evidence=None):
+    """Modify a set of factors given some evidence
+
+    Args:
+        factors (List[Factor]): List of input factors
+        evidence (Dict): Dictionary, where the keys are the observed variables
+          and the values are the observed values.
+
+    Returns:
+        List of factors after observing evidence
+    """
+    if evidence is None:
+        return factors
+    out = copy.deepcopy(factors)
+
+    """ YOUR CODE HERE
+    Set the probabilities of assignments which are inconsistent with the 
+    evidence to zero.
+    """
+
+    return out
+
+
+"""For max sum meessage passing (for MAP)"""
+def factor_sum(A, B):
+    """Same as factor_product, but sums instead of multiplies
+    """
+    if A.is_empty():
+        return B
+    if B.is_empty():
+        return A
+
+    # Create output factor. Variables should be the union between of the
+    # variables contained in the two input factors
+    out = Factor()
+    out.var = np.union1d(A.var, B.var)
+
+    # Compute mapping between the variable ordering between the two factors
+    # and the output to set the cardinality
+    out.card = np.zeros(len(out.var), np.int64)
+    mapA = np.argmax(out.var[None, :] == A.var[:, None], axis=-1)
+    mapB = np.argmax(out.var[None, :] == B.var[:, None], axis=-1)
+    out.card[mapA] = A.card
+    out.card[mapB] = B.card
+
+    # For each assignment in the output, compute which row of the input factors
+    # it comes from
+    out.val = np.zeros(np.prod(out.card))
+    assignments = out.get_all_assignments()
+    idxA = assignment_to_index(assignments[:, mapA], A.card)
+    idxB = assignment_to_index(assignments[:, mapB], B.card)
+
+    """ YOUR CODE HERE
+    You should populate the .val field with the factor sum. The code for this
+    should be very similar to the factor_product().
+    """
+
+    return out
+
+
+def factor_max_marginalize(factor, var):
+    """Marginalize over a list of variables by taking the max.
+
+    Args:
+        factor (Factor): Input factor
+        var (List): Variable to marginalize out.
+
+    Returns:
+        out: Factor with variables in 'var' marginalized out. The factor's
+          .val_argmax field should be a list of dictionary that keep track
+          of the maximizing values of the marginalized variables.
+          e.g. when out.val_argmax[i][j] = k, this means that
+            when assignments of out is index_to_assignment[i],
+            variable j has a maximizing value of k.
+          See test_lab1.py::test_factor_max_marginalize() for an example.
+    """
+    out = Factor()
+
+    """ YOUR CODE HERE
+    Marginalize out the variables given in var. 
+    You should make use of val_argmax to keep track of the location with the
+    maximum probability.
+    """
+
+    return out
+
+
+def compute_joint_distribution(factors):
+    """Computes the joint distribution defined by a list of given factors
+
+    Args:
+        factors (List[Factor]): List of factors
+
+    Returns:
+        Factor containing the joint distribution of the input factor list
+    """
+    joint = Factor()
+
+    """ YOUR CODE HERE
+    Compute the joint distribution from the list of factors. You may assume
+    that the input factors are valid so no input checking is required.
+    """
+
+    return joint
+
+
+def compute_marginals_naive(V, factors, evidence):
+    """Computes the marginal over a set of given variables
+
+    Args:
+        V (int): Single Variable to perform inference on
+        factors (List[Factor]): List of factors representing the graphical model
+        evidence (Dict): Observed evidence. evidence[k] = v indicates that
+          variable k has the value v.
+
+    Returns:
+        Factor representing the marginals
+    """
+
+    output = Factor()
+
+    """ YOUR CODE HERE
+    Compute the marginal. Output should be a factor.
+    Remember to normalize the probabilities!
+    """
+
+    return output
+
+
+def compute_marginals_bp(V, factors, evidence):
+    """Compute single node marginals for multiple variables
+    using sum-product belief propagation algorithm
+
+    Args:
+        V (List): Variables to infer single node marginals for
+        factors (List[Factor]): List of factors representing the grpahical model
+        evidence (Dict): Observed evidence. evidence[k]=v denotes that the
+          variable k is assigned to value v.
+
+    Returns:
+        marginals: List of factors. The ordering of the factors should follow
+          that of V, i.e. marginals[i] should be the factor for variable V[i].
+    """
+    # Dummy outputs, you should overwrite this with the correct factors
+    marginals = []
+
+    # Setting up messages which will be passed
+    factors = observe_evidence(factors, evidence)
+    graph = generate_graph_from_factors(factors)
+
+    # Uncomment the following line to visualize the graph. Note that we create
+    # an undirected graph regardless of the input graph since 1) this
+    # facilitates graph traversal, and 2) the algorithm for undirected and
+    # directed graphs is essentially the same for tree-like graphs.
+    # visualize_graph(graph)
+
+    # You can use any node as the root since the graph is a tree. For simplicity
+    # we always use node 0 for this assignment.
+    root = 0
+
+    # Create structure to hold messages
+    num_nodes = graph.number_of_nodes()
+    messages = [[None] * num_nodes for _ in range(num_nodes)]
+
+    """ YOUR CODE HERE
+    Use the algorithm from lecture 4 and perform message passing over the entire
+    graph. Recall the message passing protocol, that a node can only send a
+    message to a neighboring node only when it has received messages from all
+    its other neighbors.
+    Since the provided graphical model is a tree, we can use a two-phase 
+    approach. First we send messages inward from leaves towards the root.
+    After this is done, we can send messages from the root node outward.
+    
+    Hint: You might find it useful to add auxilliary functions. You may add 
+      them as either inner (nested) or external functions.
+    """
+
+    return marginals
+
+
+def map_eliminate(factors, evidence):
+    """Obtains the maximum a posteriori configuration for a tree graph
+    given optional evidence
+
+    Args:
+        factors (List[Factor]): List of factors representing the graphical model
+        evidence (Dict): Observed evidence. evidence[k]=v denotes that the
+          variable k is assigned to value v.
+
+    Returns:
+        max_decoding (Dict): MAP configuration
+        log_prob_max: Log probability of MAP configuration. Note that this is
+          log p(MAP, e) instead of p(MAP|e), i.e. it is the unnormalized
+          representation of the conditional probability.
+    """
+
+    max_decoding = {}
+    log_prob_max = 0.0
+
+    """ YOUR CODE HERE
+    Use the algorithm from lecture 5 and perform message passing over the entire
+    graph to obtain the MAP configuration. Again, recall the message passing 
+    protocol.
+    Your code should be similar to compute_marginals_bp().
+    To avoid underflow, first transform the factors in the probabilities
+    to **log scale** and perform all operations on log scale instead.
+    You may ignore the warning for taking log of zero, that is the desired
+    behavior.
+    """
+
+    return max_decoding, log_prob_max
